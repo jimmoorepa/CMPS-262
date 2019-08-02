1996fd58e9c3075609701129d68228f8dc4a3372
Jim Moore <jmoore@pointpark.edu>
2019-08-01 23:03:10 -0400

    Fixed first set of bugs, not displaying all info!

--- api.js
+++ api.js
@@ -28,63 +28,76 @@ function queryData(recordSelect, queryURL) {
 	
 	//Console log the query URL.
 	console.log("Query URL = " + queryURL);
+		console.log(recordSelect);
 	
 	for (var i = 0; i < recordSelect; i++) {
 		articleCounter++;
+		console.log(articleCounter);
 		var resSection = $("<div>");
-		resSection.addClass("well");
-		resSection.attr("id", "article-well-" + articleCounter);
-		$("#well-section").append(wellSection);
+		resSection.addClass("res");
+		resSection.attr("id", "article-res-" + articleCounter);
+		$("#res-section").append(resSection);
+		
+		var subSection = $("#article-details");
+		subSection.addClass("details");
+		$("#article-details").append(subSection);
 		
 		if (data.response.docs[i].headline !== "null") {
-			$("#article-well-" + articleCounter)
+			$("#article-res-" + articleCounter)
 			.append(
 			  "<h3 class='articleHeadline'><span class='label label-primary'>" +
-              articleCounter + "</span><strong> " +
-              data.response.docs[i].headline.main + "</strong></h3>"
-			);
+              articleCounter + "J</span><strong> " +
+              data.response.docs[i].headline.main + "</strong></h3>"			    
+			);			
 		} 
 		// Then display the remaining fields in the HTML (Section Name, Date, URL)
-		$("#articleWell-" + articleCounter)
-		  .append("<h5>Section: " + data.response.docs[i].section_name + "</h5>");
-		$("#articleWell-" + articleCounter)
-		  .append("<h5>" + data.response.docs[i].pub_date + "</h5>");
-		$("#articleWell-" + articleCounter)
+		//$("#res-section").append(resSection);
+		
+		$("#article-details-" + articleCounter)
+		  .append("<h5>Section: " + data.response.docs[i].section_name + "</h5>_K");
+		$("#article-details-" + articleCounter)
+		  .append("<h5>" + data.response.docs[i].pub_date + "</h5>_L");
+		$("#article-details-" + articleCounter)
 		  .append(
 			"<a href='" + data.response.docs[i].web_url + "'>" +
-			data.response.docs[i].web_url + "</a>"
+			data.response.docs[i].web_url + "</a>_M"
 		  );
 		
 	} // End of main loop.
   }); // End of getting data.
 } // End of main function.
 
-$("#RunSearch").on("click", function() {
+$("#RunSearch").on("click", function(event) {
 	
+	event.preventDefault();
 	// Reset the article counter.
 	articleCounter = 0;
 	
 	$("#well-section").empty();
 	
-	console.log($("querystr").val().trim());
+	//$('input[name=querystr]').val();
+	//$("rectoshow").val();
+	
+	console.log($('input[name=querystr]').val());
+	console.log($('input[name=rectoshow]').val());
 	
 	// Gets the data from the form.
-	searchString = $("querystr").val().trim();
+	searchString = $('input[name=querystr]').val();
 	searchURL = apiURL + searchString;
-	displayCount = $("recordsSelect").val(); //.trim();
-	startDate = $("begdate").val(); //.trim();
-	endDate = $("enddate").val(); //.trim();
+	displayCount = $('select[name=rectoshow]').val(); 
+	startDate = $('input[name=begdate]').val(); 
+	endDate = $('input[name=enddate]').val(); 
 	
 	// Appends start date and end date to search string.
-	if ($("begdate").val() !== null) {
+	if (startDate !== null) {
 		//searchURL = searchURL + "&begin_date=" + startDate + "0101";
 	}
-	if ($("enddate").val() !== null) {
+	if (endDate !== null) {
 		//searchURL = searchURL + "&end_date=" + endDate + "0101";
 	}
 	
 	// Displays the final query URL.
-	console.log("Search URL for API: " + searchURL);
+	//console.log("Search URL for API: " + searchURL);
 	
 	// Runs the query with form data.
 	queryData(displayCount, searchURL);
